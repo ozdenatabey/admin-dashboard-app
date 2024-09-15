d43844cb759ab697e2ecc3bf13d2d28cfba2b096
ozdenatabey <ozdenatabey44@gmail.com>
2024-09-15 13:27:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold text-red-600 hover:text-yellow-400 font-raleway">
+        Test
+      </h1>
+    </>
+  );
+}
+
+export default App;
